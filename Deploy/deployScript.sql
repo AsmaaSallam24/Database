69408206749e491b4fc08f15d720f22a4385d336
AsmaaSallam24 <101255452+AsmaaSallam24@users.noreply.github.com>
2024-07-12 11:26:51 +0300

    Update deployScript.sql

--- Deploy/deployScript.sql
+++ Deploy/deployScript.sql
@@ -1,13 +1,5 @@
 USE PROD;
 
-CREATE TABLE employee (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-);
-
 INSERT INTO employee (PersonID, FirstName, LastName, Address, City)
 VALUES ('1', 'Asmaa', 'Sallam', 'Giza', 'Cairo');
 
